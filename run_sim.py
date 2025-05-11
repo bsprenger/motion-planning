c78ea1d5a688aad24307e570d1eee00632cdf504
Ben Sprenger <ben.sprenger@rogers.com>
2025-05-11 21:55:26 +0200

    chore: format run_sim with ruff

--- run_sim.py
+++ run_sim.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 from motion_planning.simulator import Simulator
 
 if __name__ == "__main__":
@@ -19,7 +20,7 @@ if __name__ == "__main__":
     for i in range(1000):
         # Set the desired 6DOF position of the end effector + gripper position
         action = np.random.randn(*sim.action_spec[0].shape) * 0.1
-        observation = sim.step(action)  
+        observation = sim.step(action)
 
         if i == 0:
             print("-- Observation --")
@@ -27,5 +28,7 @@ if __name__ == "__main__":
                 print(f"{k}: Shape [{v.shape}], Type [{v.dtype}]")
             print("-- Action --")
             action_min, action_max = sim.action_spec
-            print(f"Action: Shape [{action.shape}], Type [{action.dtype}], Min [{action_min}], Max [{action_max}]")
+            print(
+                f"Action: Shape [{action.shape}], Type [{action.dtype}], Min [{action_min}], Max [{action_max}]"
+            )
         sim.render()
